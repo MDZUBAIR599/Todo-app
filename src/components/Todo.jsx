57c2ec281056ceba1562912af5d90e8bb7128ba8
Md Zubair <mdzubair5999@gmail.com>
2022-05-18 23:57:26 +0530

    doen

---
+++ src/components/Todo.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+function Todo() {
+  return (
+    <>
+        <div className="center_div">
+        <h1>Todo List</h1>
+
+
+        </div>
+    </>
+  )
+}
+export default Todo
